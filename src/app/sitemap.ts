1f0975715af23482eb46cec21576cfc4bc012bac
rb7a <130118715+rb7a@users.noreply.github.com>
2024-08-24 02:34:42 +0800

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,5 @@
 import { type MetadataRoute } from "next";
+import * as Commerce from "commerce-kit";
 import { publicUrl } from "@/env.mjs";
 
 const Categories = [
@@ -7,63 +8,36 @@ const Categories = [
 ];
 
 type Item = MetadataRoute.Sitemap[number];
-
-const stripe = new stripe(process.env.STRIPE_SECRET_KEY);
-
-async function fetchProducts() {
-	const products = await stripe.products.list();
-	return products.data;
-  }
-
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  try {
-    if (!publicUrl) {
-      throw new Error("publicUrl is not defined");
-    }
+	const products = await Commerce.productBrowse({ first: 100 });
+	const productUrls = products.map(
+		(product) =>
+			({
+				url: `${publicUrl}/product/${product.metadata.slug}`,
+				lastModified: new Date(product.updated * 1000),
+				changeFrequency: "daily",
+				priority: 0.8,
+			}) satisfies Item,
+	);
 
-//    let products;
+	const categoryUrls = Categories.map(
+		(category) =>
+			({
+				url: `${publicUrl}/category/${category.slug}`,
+				lastModified: new Date(),
+				changeFrequency: "daily",
+				priority: 0.5,
+			}) satisfies Item,
+	);
 
-    try {
-      products = await fetchProducts();
-    } catch (error) {
-      console.error("Error fetching products:", error);
-      products = []; // Fallback to empty array if product fetch fails
-    }
-
-    const productUrls = products.map((product) => ({
-      url: `${publicUrl}/product/${product.metadata.slug}`,
-      lastModified: new Date(product.updated * 1000),
-      changeFrequency: "daily",
-      priority: 0.8,
-    } as Item));
-
-    const categoryUrls = Categories.map((category) => ({
-      url: `${publicUrl}/category/${category.slug}`,
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.5,
-    } as Item));
-
-    return [
-      {
-        url: publicUrl,
-        lastModified: new Date(),
-        changeFrequency: "always",
-        priority: 1,
-      },
-      ...productUrls,
-      ...categoryUrls,
-    ];
-  } catch (error) {
-    console.error("Error generating sitemap:", error);
-    // Return a minimal sitemap in case of error
-    return [
-      {
-        url: publicUrl || 'https://your-fallback-url.com',
-        lastModified: new Date(),
-        changeFrequency: "daily",
-        priority: 1,
-      },
-    ];
-  }
+	return [
+		{
+			url: publicUrl,
+			lastModified: new Date(),
+			changeFrequency: "always",
+			priority: 1,
+		},
+		...productUrls,
+		...categoryUrls,
+	];
 }
